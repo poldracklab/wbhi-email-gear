f6f902b43dacec832a5141c0248697846e44bd39
jbwexler <jbwexler@tutanota.com>
2024-07-01 15:35:39 -0700

    add csv attachments; fix bugs

--- run.py
+++ run.py
@@ -5,9 +5,11 @@ import flywheel_gear_toolkit
 import flywheel
 import logging
 import pip
+import os
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 from datetime import datetime, timedelta
 import pandas as pd
 from redcap import Project
@@ -25,7 +27,7 @@ from wbhiutils.constants import *
 
 log = logging.getLogger(__name__)
 
-DATAVIEW_COLUMNS = [
+DATAVIEW_COLUMNS = (
     'subject.label',
     'session.id',
     'session.tags',
@@ -38,10 +40,10 @@ DATAVIEW_COLUMNS = [
     'file.classification.Features',
     'file.classification.Measurement',
     'file.classification.Custom'
-]
+)
 
 
-def create_view_df(container, columns: list, filter=None) -> pd.DataFrame:
+def create_view_df(container, columns: tuple, filter=None) -> pd.DataFrame:
     """Get unique labels for all acquisitions in the container.
 
     This is done using a single Data View which is more efficient than iterating through
@@ -65,8 +67,9 @@ def create_view_df(container, columns: list, filter=None) -> pd.DataFrame:
 
 def create_first_dcm_df(dcm_df: pd.DataFrame) -> pd.DataFrame:
     # Sort and drop duplicates to get first file from each session
-    dcm_df = dcm_df.sort_values(by=['session.id', 'file.created'])
-    return dcm_df.drop_duplicates(subset='session.id')
+    first_df = dcm_df.copy()
+    first_df = first_df.sort_values(by=['session.id', 'file.created'])
+    return first_df.drop_duplicates(subset='session.id')
     
 
 def get_acq_path(acq: AcquisitionListOutput) -> str:
@@ -177,17 +180,22 @@ def create_just_rc_df(redcap_project: Project) -> pd.DataFrame:
 
     return pd.DataFrame(just_rc_list).sort_values("date")
 
-def create_matches_df(redcap_project: Project) -> pd.DataFrame:
-    filter_logic = "[rid] != '' and [rid] != ' '"
-    redcap_data = redcap_project.export_records(filter_logic=filter_logic)
-
-
-def send_email(subject, html_content, sender, recipients, password):
+def send_email(subject, html_content, sender, recipients, password, files=None):
     msg = MIMEMultipart()
     msg['Subject'] = subject
     msg['From'] = sender
     msg['To'] = ', '.join(recipients)
     msg.attach(MIMEText(html_content, 'html'))
+    
+    for f in files or []:
+        with open(f, "rb") as fil:
+            part = MIMEApplication(
+                fil.read(),
+                Name=os.path.basename(f)
+            )
+        # After the file is closed
+        part['Content-Disposition'] = 'attachment; filename="%s"' % os.path.basename(f)
+        msg.attach(part)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
        smtp_server.login(sender, password)
@@ -197,23 +205,35 @@ def send_email(subject, html_content, sender, recipients, password):
 
 def send_wbhi_email(
         new_matches_df: pd.DataFrame,
-        just_fw_df: pd.DataFrame,
         just_rc_df: pd.DataFrame,
+        just_fw_df: pd.DataFrame,
         site=None,
         email_tag=False) -> None:
 
+    new_matches_df_copy = new_matches_df.copy()
+    just_rc_df_copy = just_rc_df.copy()
+    just_fw_df_copy = just_rc_df.copy()
+    just_rc_df = just_rc_df[just_rc_df["site"] == site]
+    
     if site:
-        try:
-            new_matches_df = new_matches_df[new_matches_df["site"] == site]
-            just_fw_df = just_fw_df[just_fw_df["site"] == site]
-            just_rc_df = just_rc_df[just_rc_df["site"] == site]
-        except KeyError:
-            pass
+        if not new_matches_df_copy.empty:
+            new_matches_df_copy = new_matches_df_copy[new_matches_df_copy["site"] == site]
+        if not just_rc_df_copy.empty:
+            just_rc_df_copy = just_rc_df_copy[just_rc_df_copy["site"] == site]
+        if not just_fw_df_copy.empty:
+            just_fw_df_copy = just_fw_df_copy[just_fw_df_copy["site"] == site]
 
-    new_matches_html = new_matches_df.to_html(index=False)
-    just_fw_html = just_fw_df.to_html(index=False)
-    just_rc_html = just_rc_df.to_html(index=False)
+    new_matches_html = new_matches_df_copy.to_html(index=False)
+    just_rc_html = just_rc_df_copy.to_html(index=False)
+    just_fw_html = just_fw_df_copy.to_html(index=False)
 
+    csv_path = os.path.join(os.environ['FLYWHEEL'], 'csv')
+    if not os.path.exists(csv_path):
+        os.makedirs(csv_path)
+    new_matches_df_copy.to_csv(os.path.join(csv_path, 'matches.csv'), index=False)
+    just_rc_df_copy.to_csv(os.path.join(csv_path, 'redcap_unmatched.csv'), index=False)
+    just_fw_df_copy.to_csv(os.path.join(csv_path, 'flywheel_unmatched.csv'), index=False)
+    
     html_content = f"""
         <p>Hello,</p>
         <p>This is a weekly summary of matches between Flywheel sessions and REDCap records.</p>
@@ -230,17 +250,21 @@ def send_wbhi_email(
         <p>Best,</p>
         <p>WBHI Team</p>
     """
+    email_list = ["jbwexler@stanford.edu"]
+    csv_list = ['matches.csv', 'redcap_unmatched.csv', 'flywheel_unmatched.csv']
+
     send_email(
         "Weekly WBHI Summary",
         html_content,
         config["gmail_address"],
-        ["jbwexler@stanford.edu"],
+        email_list,
         # ["jbwexler@stanford.edu", "buckholtz@stanford.edu", "cmtaylor@ucsb.edu", "markiewicz@stanford.edu"],
-        config["gmail_password"]
+        config["gmail_password"],
+        [os.path.join(csv_path, basename) for basename in csv_list]
     )
 
-    if email_tag and not new_matches_df.empty:
-        for index, ses_id in new_matches_df["ses_id"].items():
+    if email_tag and not new_matches_df_copy.empty:
+        for index, ses_id in new_matches_df_copy["ses_id"].items():
             session = client.get_session(ses_id)
             session.add_tag('email')
 
@@ -254,11 +278,11 @@ def main():
     new_matches_df = create_new_matches_df()
     just_fw_df = create_just_fw_df()
     just_rc_df = create_just_rc_df(redcap_project)
-    last_week_matches = create_matches_df(redcap_project)
-    send_wbhi_email(new_matches_df, just_fw_df, just_rc_df, email_tag=True)
+    send_wbhi_email(new_matches_df, just_rc_df, just_fw_df, email_tag=True)
 
     for site in SITE_LIST:
-        send_wbhi_email(new_matches_df, just_fw_df, just_rc_df, site=site)
+        send_wbhi_email(new_matches_df, just_rc_df, just_fw_df, site=site)
+        breakpoint()
 
 if __name__ == "__main__":
     with flywheel_gear_toolkit.GearToolkitContext() as gtk_context:
